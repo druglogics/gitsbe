c0ff973b88a03f0743482dba508017eb3984a004
john <bblodfon@gmail.com>
2018-08-10 15:28:59 +0200

    Added new class for parsing Launcher's command line arguments and 3 tests

---
+++ src/main/java/eu/druglogics/gitsbe/util/CommandLineArgs.java
@@ -0,0 +1,50 @@
+package eu.druglogics.gitsbe.util;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parameters(separators = "=")
+public class CommandLineArgs {
+
+    @Parameter
+    private List<String> parameters = new ArrayList<>();
+
+    @Parameter(names = { "--project", "-p" }, required = true, description = "Name of the project", order = 0)
+    private String projectName;
+
+    @Parameter(names = { "--network", "-n" }, required = true, description = "Network file", order = 1)
+    private String filenameNetwork;
+
+    @Parameter(names = { "--trainingdata", "-t" }, required = true, description = "Training data file", order = 2)
+    private String filenameTrainingData;
+
+    @Parameter(names = { "--config", "-c" }, required = true, description = "Configuration file", order = 3)
+    private String filenameConfig;
+
+    @Parameter(names = { "--modeloutputs", "-m" }, required = true, description = "Model outputs file", order = 4)
+    private String filenameModelOutputs;
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public String getFilenameNetwork() {
+        return filenameNetwork;
+    }
+
+    public String getFilenameTrainingData() {
+        return filenameTrainingData;
+    }
+
+    public String getFilenameConfig() {
+        return filenameConfig;
+    }
+
+    public String getFilenameModelOutputs() {
+        return filenameModelOutputs;
+    }
+
+}
---
+++ src/test/java/eu/druglogics/gitsbe/util/CommandLineArgsTest.java
@@ -0,0 +1,46 @@
+package eu.druglogics.gitsbe.util;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class CommandLineArgsTest {
+
+    @Test
+    public void test_all_arguments() {
+        CommandLineArgs args = new CommandLineArgs();
+
+        String[] argv = {"-p", "project", "-c", "config", "-m", "model", "-t", "train", "-n", "network"};
+        JCommander.newBuilder().addObject(args).build().parse(argv);
+
+        Assert.assertEquals(args.getProjectName(), "project");
+        Assert.assertEquals(args.getFilenameNetwork(), "network");
+        Assert.assertEquals(args.getFilenameConfig(), "config");
+        Assert.assertEquals(args.getFilenameModelOutputs(), "model");
+        Assert.assertEquals(args.getFilenameTrainingData(), "train");
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void test_no_arguments() {
+        exception.expect(ParameterException.class);
+
+        CommandLineArgs args = new CommandLineArgs();
+        JCommander.newBuilder().addObject(args).build().parse("");
+    }
+
+    @Test
+    public void test_missing_required_arguments() {
+        exception.expect(ParameterException.class);
+
+        CommandLineArgs args = new CommandLineArgs();
+        String[] argv = {"-p", "project", "-c", "config"};
+        JCommander.newBuilder().addObject(args).build().parse(argv);
+    }
+
+}
