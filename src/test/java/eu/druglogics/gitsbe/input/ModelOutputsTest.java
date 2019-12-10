f9a5b3019a17ffc4d725a0f4758e57c533869866
john <bblodfon@gmail.com>
2019-12-10 14:44:32 +0100

    more precise testing of globaloutput values

--- src/test/java/eu/druglogics/gitsbe/input/ModelOutputsTest.java
+++ src/test/java/eu/druglogics/gitsbe/input/ModelOutputsTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -79,42 +80,48 @@ class ModelOutputsTest {
         // check that the value of global output is between 0 and 1
         ArrayList<String> stableStates = new ArrayList<>();
         stableStates.add("00000" + "000" + "000"); // none is active
-        assertThat(modeloutputs.calculateGlobalOutput(stableStates, booleanModel))
-                .isBetween((float) 0, (float) 1);
+
+        DecimalFormat df = new DecimalFormat("#.00000");
+        Double gl1 = Double.valueOf(df.format(modeloutputs.calculateGlobalOutput(stableStates, booleanModel)));
+        assertEquals(gl1, 0.33333);
 
         stableStates.clear();
         stableStates.add("00000" + "100" + "000"); // F is active
-        assertThat(modeloutputs.calculateGlobalOutput(stableStates, booleanModel))
-                .isBetween((float) 0, (float) 1);
+        Double gl2 = Double.valueOf(df.format(modeloutputs.calculateGlobalOutput(stableStates, booleanModel)));
+        assertEquals(gl2, 0.66667);
 
         stableStates.clear();
         stableStates.add("00000" + "010" + "000"); // U is active
-        assertThat(modeloutputs.calculateGlobalOutput(stableStates, booleanModel))
-                .isEqualTo((float) 0); // the minimum (normalized) global output: -1
+        Double gl3 = Double.valueOf(df.format(modeloutputs.calculateGlobalOutput(stableStates, booleanModel)));
+        assertEquals(gl3, 0.0); // the minimum (normalized) global output: -1
 
         stableStates.clear();
         stableStates.add("00000" + "011" + "000"); // U and K are active
-        assertThat(modeloutputs.calculateGlobalOutput(stableStates, booleanModel))
-                .isBetween((float) 0, (float) 1);
+        Double gl4 = Double.valueOf(df.format(modeloutputs.calculateGlobalOutput(stableStates, booleanModel)));
+        assertEquals(gl4, 0.33333);
 
         stableStates.clear();
         stableStates.add("00000" + "101" + "000"); // F and K are active
-        assertThat(modeloutputs.calculateGlobalOutput(stableStates, booleanModel))
-                .isEqualTo((float) 1); // the maximum (normalized) global output: 2
+        Double gl5 = Double.valueOf(df.format(modeloutputs.calculateGlobalOutput(stableStates, booleanModel)));
+        assertEquals(gl5, 1.0); // the maximum (normalized) global output: 2
 
         stableStates.clear();
         stableStates.add("00000" + "111" + "000"); // F, U and K (all) are active
-        assertThat(modeloutputs.calculateGlobalOutput(stableStates, booleanModel))
-                .isBetween((float) 0, (float) 1);
+        Double gl6 = Double.valueOf(df.format(modeloutputs.calculateGlobalOutput(stableStates, booleanModel)));
+        assertEquals(gl6, 0.66667);
 
         // check that the value of global output is between
         // 0 and 1 in the case of multiple stable states
         stableStates.add("00000" + "101" + "000");
         assertThat(modeloutputs.calculateGlobalOutput(stableStates, booleanModel))
                 .isBetween((float) 0, (float) 1);
+        Double gl7 = Double.valueOf(df.format(modeloutputs.calculateGlobalOutput(stableStates, booleanModel)));
+        assertEquals(gl7, 0.83333);
 
         stableStates.add("00000" + "100" + "000");
         assertThat(modeloutputs.calculateGlobalOutput(stableStates, booleanModel))
                 .isBetween((float) 0, (float) 1);
+        Double gl8 = Double.valueOf(df.format(modeloutputs.calculateGlobalOutput(stableStates, booleanModel)));
+        assertEquals(gl8, 0.77778);
     }
 }
